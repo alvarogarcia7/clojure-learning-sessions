2c2dba9d91fb4884888dcbac7da2d7a1bca666ba
alvaro garcia <derecerca@gmail.com>
2015-02-06 01:40:04 +0100

    add a title and source

--- 01-functions.clj
+++ 01-functions.clj
@@ -1,3 +1,11 @@
+;; a bit about clojure functions
+; main source: https://clojuredocs.org/clojure.core/defn
+
+; see (source defn)
+
+; defn is the same as (def name (fn [params* ] exprs*)) or
+; (def name (fn ([params* ] exprs*)+))
+
 ; this is a function without parameters
 (defn no-params []
   8)
